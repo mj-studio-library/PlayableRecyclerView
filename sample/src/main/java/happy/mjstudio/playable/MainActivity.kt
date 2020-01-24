560875e020a813b3c61b32e05e49bb028795def4
MJ Studio <mym0404@gmail.com>
2020-01-25 00:14:07 +0900

    Update Sample UI

--- sample/src/main/java/happy/mjstudio/playable/MainActivity.kt
+++ sample/src/main/java/happy/mjstudio/playable/MainActivity.kt
@@ -13,44 +13,38 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
 
-    private val sampleDatas = listOf(
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_c_choreography.mp4",
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_c_choreography.png"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/1579496605382_merged.mp4_1579496619388.mp4",
-            "https://cdn.staging.beatflo.co/video/thumbnail.jpg_1579496619819.mp4"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_b_choreography.mp4",
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_b_choreography.png"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_c_choreography.mp4",
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_c_choreography.png"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/1579496605382_merged.mp4_1579496619388.mp4",
-            "https://cdn.staging.beatflo.co/video/thumbnail.jpg_1579496619819.mp4"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_b_choreography.mp4",
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_b_choreography.png"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_c_choreography.mp4",
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_c_choreography.png"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/1579496605382_merged.mp4_1579496619388.mp4",
-            "https://cdn.staging.beatflo.co/video/thumbnail.jpg_1579496619819.mp4"
-        ),
-        SamplePlayable(
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_b_choreography.mp4",
-            "https://cdn.staging.beatflo.co/video/trippin_budy_part_b_choreography.png"
-        )
+    private val sampleThumbs = listOf(
+
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerEscapes.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/SubaruOutbackOnStreetAndDirt.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg"
+
     )
+    private val sampleVideos = listOf(
+
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+
+    )
+
+    private val sampleDatas = sampleVideos.indices.map {
+        SamplePlayable(sampleVideos[it], sampleThumbs[it])
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- sample/src/main/java/happy/mjstudio/playable/SamplePlayable.kt
+++ sample/src/main/java/happy/mjstudio/playable/SamplePlayable.kt
@@ -16,7 +16,9 @@ data class SamplePlayable(
         "I am happy to introduce my PlayableRecyclerView",
         "Revolutional Player List Library",
         "No Memory Leak",
-        "Fast, Robust!"
+        "Fast, Robust!",
+        "Android hasn't native feature show list of videos. Believe me. Use PlayableRecyclerView",
+        "This is my third Android library"
     ).random(),
     val location: String = listOf(
         "Gimpo",
@@ -27,7 +29,10 @@ data class SamplePlayable(
         "Stanford"
     ).random(),
     val description: String = listOf(
-        "This is MJStargram's description. It need to be long but we can't",
-        "Who want this library"
-    ).random()
+        "This is MJStargram's description. It need to be long but we can't\nWho wants this library\nThis is Veeeery Robust and Absolutely Fast\uD83E\uDDE9",
+        "Who wants this library? The architecture of this library is not hard to read.\nI will create docs for this.🔮",
+        "Great ideas do not rust. ⚔️",
+        "This is just an alpha pre-release.\nWait me ⭕️",
+        "Magic with Kotlin ⚓️"
+    ).let { "${it.random()}\n${it.random()}" }
 ) : Parcelable
