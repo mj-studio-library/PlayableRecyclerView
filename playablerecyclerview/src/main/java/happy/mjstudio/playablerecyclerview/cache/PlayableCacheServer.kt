c189cba5487d171b398a2ac79f889e2e7a89b882
MJ Studio <mym0404@gmail.com>
2020-01-29 04:15:42 +0900

    Create - PlayableCacheServer

---
+++ playablerecyclerview/src/main/java/happy/mjstudio/playablerecyclerview/cache/PlayableCacheServer.kt
@@ -0,0 +1,15 @@
+package happy.mjstudio.playablerecyclerview.cache
+
+import android.app.Application
+
+/**
+ * Created by mj on 29, January, 2020
+ */
+class PlayableCache private constructor() {
+
+    companion object {
+        fun getInstance(context: Application) {
+
+        }
+    }
+}
